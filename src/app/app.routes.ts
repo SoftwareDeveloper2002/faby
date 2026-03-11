7ba8105d64c1d6d8c63dd6a184fdaa8f5f955879
Robby Roda <robbyroda.00@gmail.com>
2026-03-11 09:59:12 +0800

    sh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Maintenance } from './pages/maintenance/maintenance';
 import { Notfound } from './pages/notfound/notfound';
 
 export const routes: Routes = [
-  { path: '', component: Maintenance },
-  { path: '**', component: Notfound },
+  { path: '', component: Maintenance, title: 'Faby | Coming Soon' },
+  { path: '**', component: Notfound, title: '404 | Page Not Found' },
 ];
